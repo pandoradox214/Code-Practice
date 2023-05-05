aac7b7776fe68fd304e83f1af92a05fa41b5e212
pandoradox214 <106655845+pandoradox214@users.noreply.github.com>
2023-05-06 00:33:45 +0800

    Added Rock Paper Scissors Game and dummy html on Practice 2

---
+++ webApp/Practice 2/script/gamelogicPart2.js
@@ -0,0 +1,86 @@
+function clickRock(){
+    const combinations = {
+        Rock: 1/3,
+        Paper: 2/3,
+        Scissors: 3/3
+    }
+    const humanChoice = combinations.Rock;
+    const ComputerChoice = Math.random();
+    if (ComputerChoice < combinations.Rock){
+        computerCalculatedChoice = combinations.Rock;
+        console.log("Computer chose rock");
+    }
+    else if (ComputerChoice < combinations.Paper && ComputerChoice >= combinations.Rock){
+        computerCalculatedChoice = combinations.Paper;
+        console.log("Computer chose paper");
+    }
+    else if(ComputerChoice >= combinations.Paper){
+        computerCalculatedChoice = combinations.Scissors;
+        console.log("Computer chose scissors");
+    }
+    winState(humanChoice, computerCalculatedChoice);
+}
+
+function clickPaper(){
+    const combinations = {
+        Rock: 1/3,
+        Paper: 2/3,
+        Scissors: 3/3
+    }
+    const humanChoice = combinations.Paper;
+    const ComputerChoice = Math.random();
+    if (ComputerChoice < combinations.Rock){
+        computerCalculatedChoice = combinations.Rock;
+        console.log("Computer chose rock");
+    }
+    else if (ComputerChoice < combinations.Paper && ComputerChoice >= combinations.Rock){
+        computerCalculatedChoice = combinations.Paper;
+        console.log("Computer chose paper");
+    }
+    else if(ComputerChoice >= combinations.Paper){
+        computerCalculatedChoice = combinations.Scissors;
+        console.log("Computer chose scissors");
+    }
+    winState(humanChoice, computerCalculatedChoice);
+}
+
+function clickScissors(){
+    const combinations = {
+        Rock: 1/3,
+        Paper: 2/3,
+        Scissors: 3/3
+    }
+    const humanChoice = combinations.Scissors;
+    const ComputerChoice = Math.random();
+    if (ComputerChoice < combinations.Rock){
+        computerCalculatedChoice = combinations.Rock;
+        console.log("Computer chose rock");
+    }
+    else if (ComputerChoice < combinations.Paper && ComputerChoice >= combinations.Rock){
+        computerCalculatedChoice = combinations.Paper;
+        console.log("Computer chose paper");
+    }
+    else if(ComputerChoice >= combinations.Paper){
+        computerCalculatedChoice = combinations.Scissors;
+        console.log("Computer chose scissors");
+    }
+    winState(humanChoice, computerCalculatedChoice);
+}
+
+function winState(humanChoice, computerCalculatedChoice){
+    if(humanChoice == 1/3 && computerCalculatedChoice == 3/3){
+        console.log("You win!");
+    }
+    else if(humanChoice == 2/3 && computerCalculatedChoice == 1/3){
+        console.log("You win!");
+    }
+    else if(humanChoice == 3/3 && computerCalculatedChoice == 2/3){
+        console.log("You win!");
+    }
+    else if(humanChoice == computerCalculatedChoice){
+        console.log("It's a tie!");
+    }
+    else{
+        console.log("You lose!");
+    }
+}
